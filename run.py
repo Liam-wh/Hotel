71d29e51beb62e34b541f0542b0dc9d7ce6664ca
WLiam-Dev <jadosone2@gmail.com>
2025-06-11 02:56:39 -0400

    commit

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('pagina/index.html')
+    return render_template('base.html')
 
 @app.route('/bienvenido')
 def bienvenido():
