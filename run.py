5cd70607f215610a947d10efc13e2dfb81293de9
WLiam-Dev <jadosone2@gmail.com>
2025-06-11 02:43:41 -0400

    mejorado 2

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('pagina/index.html')
 
 @app.route('/bienvenido')
 def bienvenido():
